dca66b2c6efe7345c07c78c5047b0bdeff6a3e8b
Muzammil <hasanmuzammil2812@gmail.com>
2023-09-04 08:28:52 +0530

    changes in send email

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,13 +1,59 @@
-import sendEmail from "@/lib/utils";
-import {NextApiRequest, NextApiResponse} from "next";
+// import sendEmail from "@/lib/utils";
+// import {NextApiRequest, NextApiResponse} from "next";
 
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
-) {
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse,
+// ) {
 
-    sendEmail()
+//     sendEmail()
 
-    res.status(200).json(`results`);
+//     res.status(200).json(`results`);
+// }
+
+import nodemailer from 'nodemailer';
+import { NextApiResponse,NextApiRequest } from 'next';
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'bad47780a32f4a',
+    pass: 'aee290a5a83646',
+  },
+});
+
+async function sendEmail() {
+  const mailOptions = {
+    from: 'your_email@example.com',
+    to: 'hasanmuzammil2812@gmail.com',
+    subject: 'cronjobs',
+    text: 'Check email sending',
+  };
+
+  try {
+    console.log('Email sent successfully');
+    return  transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+export default async function handler(req : NextApiRequest,res:NextApiResponse){
+  if(req.method === 'POST'){
+    const emailRes = await sendEmail()
+    if(emailRes?.messageId){
+      return res.status(200).json({message : 'email send successfully'})
+    }
+    return res.status(400).json({ message: 'Error sending email' });
+  }
+  if(req.method === 'GET'){
+    const emailRes = await sendEmail()
+    if(emailRes?.messageId){
+      return res.status(200).json({message : 'email send successfully'})
+    }
+    return res.status(400).json({ message: 'Error sending email' });
+  }
+  return res.status(400).json({ message: `Incorrect method: ${req.method}. Did you mean POST?` });
 }
